786300721c636f3bf10e573582518b9a98e25748
Matthew Neeley <u0854535@notchpeak2.int.chpc.utah.edu>
2021-08-10 14:43:47 -0600

    added .gitignore

--- work/7a/ec028d1a1f4c64badd24d6504853aa/.command.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -ue
-fastqc -f fastq gut_1.fq
-fastqc -f fastq gut_2.fq
--- work/99/7bf30a48d7aef0398ee939b3c1cb92/.command.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -ue
-module load fastqc
-fastqc -f fastq gut_1.fq > result1
-fastqc -f fastq gut_2.fq > result2
