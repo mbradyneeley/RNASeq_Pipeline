7a002545d80e0dd00dd3fe5fc7575d6dd5cdc473
Matthew Neeley <u0854535@lonepeak1.wasatch.peaks>
2021-08-17 10:18:52 -0600

    updated the STAR command to include the readFileCommand option so that the zipped fastqs can be read in. I also set it up to output the large bam files to scratch.

--- runTheBifrost.sh
+++ runTheBifrost.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-#SBATCH --time=3-00:00:00
+#SBATCH --time=06:00:00
 #SBATCH --nodes=1
 #SBATCH -o pipelineKickOff-%j.out
 #SBATCH -e pipelineKickOff-%j.err
@@ -16,12 +16,6 @@ resume=$1
 configFile=$2
 # rename this vvvv to create a new directory for your project (i.e. if you want to start over without deleting what you've already done)
 scratchDir="/scratch/general/lustre/$USER/theBifrost"
-#var=$(cat nextflow.config | grep "scratch =")--|
-#IFS="\"" read -ra varArr <<< $var              |- cant get to work yet
-#scratchDir=${varArr[1]}                      --|
-# IGNORE: rename this vvvv to change clusters (e.g. kingspeak, notchpeak, ember, lonepeak, etc.)
-# IGNORE: SLURM_CLUSTERS="notchpeak"
-# IGNORE: export SLURM_CLUSTERS
 
 if [[ $resume == "resume" ]]; then
     if [ -d $scratchDir ]; then
